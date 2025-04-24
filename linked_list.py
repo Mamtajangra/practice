cc6c3d1eafaa7c81fb1f629158b4881183f07247
Mamta Jangra <mamtajangra204@gmail.com>
2025-04-24 18:05:33 +0530

    FIND TARGET INDICES AND SORT ARRAY

--- linked_list.py
+++ linked_list.py
@@ -33,6 +33,21 @@ class Linked_list:
         # add head to new node 
         self.head = new_node
 
+# define append function to add values at last of linked list 
+    def append(self,value):
+        # make this data a new node 
+        new_node = Node(value)
+# if list is already empty new node is initial node 
+        if self.head is None:
+            self.head = new_node
+            return 
+        # otherwise we check upto last 
+        temp = self.head
+        # here we will check upto last then add new node there 
+        while temp.next:
+            temp = temp.next
+        temp.next = new_node    
+
 
 # obtain node 
 n1 = Node(10)
@@ -43,6 +58,7 @@ n5 = Node(50)
 
 # let head = n1 
 head = n1
+# linkage 
 n1.next = n2
 n2.next = n3
 n3.next = n4
@@ -57,10 +73,11 @@ LL1.insert(100)
 LL1.insert(110)
 LL1.insert(120)
 LL1.insert(130)
-LL1.insert(140)
+LL1.insert(140)          ## insert values at the beginning 
+LL1.append(500)          ##append 
 
 
-print("after insertion at beginning")
+print("after insertion at start and last")
 
 # traversing after adding new node 
 LL1.traverse()
