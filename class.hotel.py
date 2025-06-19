d0810d6dad28c674aacdd2245292ff4ca82312ff
Mamta Jangra <mamtajangra204@gmail.com>
2025-06-19 23:13:23 +0530

    Refactor spacing and formatting in Hotel class for improved readability

--- class.hotel.py
+++ class.hotel.py
@@ -4,10 +4,10 @@ class Hotel:
 
     # define constructor that determine the properties  and feature of class
     def __init__(self,place,rooms,staff,name="the green heaven"):
-        self.place=place
-        self.rooms=rooms
-        self.staff=staff
-        self.name=name
+        self.place = place
+        self.rooms = rooms
+        self.staff = staff
+        self.name = name
 
 
         # a method is constructed 
@@ -15,7 +15,7 @@ class Hotel:
         print(f"the hotel is in {self.place},there are {self.rooms}rooms,the number of staff are{self.staff},and the name of the hotel is {self.name}")    
 
 # formation of object of class 
-hotel1=Hotel("india","5000","10000","the green heaven")
+hotel1 = Hotel("india","5000","10000","the green heaven")
 
 
 
